ddf9525a519d922bccf9c6f6c400bf45a2a96e4a
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-11-24 15:59:48 +0200

    Update slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const swiper = new Swiper(".swiper", {
+  effect: "fade",
+  loop: true,
+
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
   },
 });
