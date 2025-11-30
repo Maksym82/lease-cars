911a273510b754262cd78e611abc4502fc2e898d
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-11-30 10:18:02 +0200

    Update  new-cars

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// Tabs
+const tabItem = document.querySelectorAll(".tabs__btn-item");
+const tabContent = document.querySelectorAll(".tabs__content-item");
+
+tabItem.forEach(function (element) {
+  element.addEventListener("click", open);
+});
+
+function open(event) {
+  const tabTarget = event.currentTarget;
+
+  tabItem.forEach(function (item) {
+    item.classList.remove("tabs__btn-item--active");
+  });
+
+  tabTarget.classList.add("tabs__btn-item--active");
+}
+
+// Swiper
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   loop: true,
