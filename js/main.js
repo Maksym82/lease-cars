44854a43831ae29a4a914228b31ce1511f0e6492
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-11-30 10:58:26 +0200

    Update styles new-cars

--- js/main.js
+++ js/main.js
@@ -8,12 +8,19 @@ tabItem.forEach(function (element) {
 
 function open(event) {
   const tabTarget = event.currentTarget;
+  const button = tabTarget.dataset.button;
 
   tabItem.forEach(function (item) {
     item.classList.remove("tabs__btn-item--active");
   });
 
   tabTarget.classList.add("tabs__btn-item--active");
+
+  tabContent.forEach(function (item) {
+    item.classList.remove("tabs__content-item--active");
+  });
+
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active')
 }
 
 // Swiper
